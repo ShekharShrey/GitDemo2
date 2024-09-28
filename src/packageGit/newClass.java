5b96cf821b44ad11393efb2eac347134afa2e0ed
ShekharShrey <shekhar.shreyshekhar2@gmail.com>
2024-09-28 14:44:50 +0530

    Recommit

--- src/packageGit/newClass.java
+++ src/packageGit/newClass.java
@@ -5,6 +5,8 @@ public class newClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("All dev done");
+		System.out.println("Not seen till now");
+		System.out.println("Let's check now");
 
 	}
 
